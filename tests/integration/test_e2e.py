a4b0a0a5372d4fda68cc411bb1a0c6993362a807
Maria Öhrman <maria.ohrman@gu.se>
2025-08-20 14:20:21 +0200

    fix: use syrupy snaphots in test_e2e

--- tests/integration/test_e2e.py
+++ tests/integration/test_e2e.py
@@ -2,89 +2,51 @@ from karps.models import SearchResult, UserErrorResult
 from tests.integration.conftest import Backend
 
 
-def test_search(backend: Backend):
-    """
-    This test depends on a resource called ao available with word as str and non-collection
-    """
-    resource_id = "ao"
-    entries = backend.search([resource_id], "q=equals|word|eventuell")
-    assert entries.total > 0
-    assert entries.resource_hits[resource_id] > 0
+"""
+These tests depend on ao, fulaord and kelly
+"""
+
+def test_search(backend: Backend, snapshot):
+    entries = backend.search(["ao"], "q=equals|word|eventuell")
+    assert entries == snapshot
 
 
-def test_search_with_from(backend: Backend):
+def test_search_with_from(backend: Backend, snapshot):
     result = backend.search(["ao", "kelly"], "q=equals|partOfSpeech|nn")
-    # assert expected size
-    assert len(result.hits) == 10
-    # assert expected totals
-    assert result.total > 10
-    # assert the number of hits are the expected size
-    assert len(result.hits) == 10
-    # for each hit, check that resource_id is correct
-    for hit in result.hits:
-        assert hit.resource_id == "ao"
+    assert result == snapshot
 
-    # use resource_hits to find where the ao results start
+
+def test_search_three_resources(backend: Backend, snapshot):
+    # should give one fulaord and 9 kelly
+    result = backend.search(["kelly", "ao", "fulaord"], "q=startswith|word|a", from_=577)
+    assert result == snapshot
+
+
+def test_search_three_resources2(backend: Backend, snapshot):
+    result = backend.search(["ao", "kelly", "fulaord"], "q=startswith|word|a")
     ao_count = result.resource_hits["ao"]
-    # we expected 1 entry from ao and 9 from kelly
-    result2 = backend.search(["ao", "kelly"], "q=equals|partOfSpeech|nn", from_=ao_count - 1)
-    assert len(result2.hits) == 10
-    assert result2.total > 10
-
-    # first hit should be ao
-    assert result.hits[0].resource_id == "ao"
-    # the rest should be kelly
-    for hit in result2.hits[1:]:
-        assert hit.resource_id == "kelly"
-
-    # resource_hits and total should be the same no matter what page we are fetching
-    assert result.resource_hits == result2.resource_hits
-    assert result.total == result2.total
+    kelly_count = result.resource_hits["kelly"]
+    result = backend.search(["kelly", "ao", "fulaord"], "q=startswith|word|a", from_=ao_count + kelly_count)
+    assert result == snapshot
 
 
-def test_search_three_resources(backend: Backend):
-    # should give one fulaord and 9 kelly
-    result = backend.search(["kelly", "ao", "fulaord"], "q=startswith|word|a&from=577")
-    assert len(result.hits) == 10
-    assert result.resource_order == ["ao", "fulaord", "kelly"]
-    assert result.hits[0].resource_id == "fulaord"
-    for hit in result.hits[1:]:
-        assert hit.resource_id == "kelly"
-
-
-def test_search_three_resources2(backend: Backend):
-    # should give one fulaord and 9 kelly
-    result = backend.search(["kelly", "ao", "fulaord"], "q=startswith|word|a&from=578")
-    assert len(result.hits) == 10
-    assert result.resource_order == ["ao", "fulaord", "kelly"]
-    for hit in result.hits[0:]:
-        assert hit.resource_id == "kelly"
-
-
-def test_search_with_too_large_from(backend: Backend):
+def test_search_with_too_large_from(backend: Backend, snapshot):
     result = backend.search(["ao", "kelly"], "q=equals|partOfSpeech|nn")
     ao_count = result.resource_hits["ao"]
     kelly_count = result.resource_hits["kelly"]
+
     result, status = backend.search_with_status(
         ["ao", "kelly"], "q=equals|partOfSpeech|nn", from_=ao_count + kelly_count
     )
-    assert isinstance(result, UserErrorResult)
-    assert status == 500
-    assert "from" in result.message
+    assert result, status == snapshot
 
 
-def test_zero_hits(backend: Backend):
+def test_zero_hits(backend: Backend, snapshot):
     result, status = backend.search_with_status(["ao", "kelly"], "q=equals|partOfSpeech|ASDFGHJKL")
-    assert isinstance(result, SearchResult)
-    ao_count = result.resource_hits["ao"]
-    kelly_count = result.resource_hits["kelly"]
-    assert ao_count == kelly_count == 0
-    assert status != 500
-    assert result.hits == []
+    assert result, status == snapshot
 
 
-def test_count(backend: Backend):
+def test_count(backend: Backend, snapshot):
     resource_id = "ao"
     entries = backend.count([resource_id], "q=equals|word|eventuell")
-    assert entries.headers == ["word", "ao", "total"]
-    assert len(entries.table) > 0
+    assert entries == snapshot
