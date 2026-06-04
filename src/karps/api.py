9104be504475bd3544ca9799b8368ee1185afb1f
Maria Öhrman <maria.ohrman@gu.se>
2026-06-04 14:20:47 +0200

    correct description of query lang

--- src/karps/api.py
+++ src/karps/api.py
@@ -199,7 +199,7 @@ def get_q_param():
     return Query(
         None,
         title="Query",
-        description='The query. See http://ws.spraakbanken.gu.se/ws/karp/v7 for a description of the query language, however, Karp-s does not support nesting boolean queries, the "not"-boolean query, sub-queries, exists, missing and freetext.',
+        description="The query. See https://ws.spraakbanken.gu.se/docs/karp for a description of the query language, however, Karp-s does not support sub-queries, exists, missing and freetext.",
     )
 
 
